fb8d14a24b2aa70a382a52e1380e6a5537c67dbb
Tendai Kastande <tendaikatsande@live.com>
2022-09-19 10:53:57 +0200

    Extended timeout for HTTP in Bot Module

--- src/resources/bot/bot.module.ts
+++ src/resources/bot/bot.module.ts
@@ -1,24 +1,24 @@
-import { Bot, BotSchema } from './entities/bot.entity';
-import { Module } from '@nestjs/common';
-import { BotService } from './bot.service';
-import { BotController } from './bot.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { BotAccountService } from 'src/providers/bot-account/bot-account.service';
-import { UserModule } from '../user/user.module';
-import { SmsService } from 'src/providers/sms/sms.service';
-import { HttpModule } from '@nestjs/axios';
-import { ListingModule } from '../listing/listing.module';
-import { TenantModule } from '../tenant/tenant.module';
-import { LeaseModule } from '../lease/lease.module';
-import { SubscriptionTypeModule } from '../subscription-type/subscription-type.module';
-import { WhatsappService } from 'src/providers/whatsapp/whatsapp.service';
+import { Bot, BotSchema } from "./entities/bot.entity";
+import { Module } from "@nestjs/common";
+import { BotService } from "./bot.service";
+import { BotController } from "./bot.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { BotAccountService } from "src/providers/bot-account/bot-account.service";
+import { UserModule } from "../user/user.module";
+import { SmsService } from "src/providers/sms/sms.service";
+import { HttpModule } from "@nestjs/axios";
+import { ListingModule } from "../listing/listing.module";
+import { TenantModule } from "../tenant/tenant.module";
+import { LeaseModule } from "../lease/lease.module";
+import { SubscriptionTypeModule } from "../subscription-type/subscription-type.module";
+import { WhatsappService } from "src/providers/whatsapp/whatsapp.service";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Bot.name, schema: BotSchema }]),
     HttpModule.registerAsync({
       useFactory: () => ({
-        timeout: 5000,
+        timeout: 15000,
         maxRedirects: 5,
       }),
     }),
