6c985187a9aaa700af2f87c6c1655c3e6dbb2e1b
Tendai Katsande <tendaikatsande@live.com>
2022-09-20 09:25:37 +0200

    Spelling adjustements

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -347,7 +347,7 @@ export const EnTranslations = {
   },
 
   GET_SUBSCRIPTION_START_DATE: {
-    title: 'When do should the subscription start?',
+    title: 'When should the subscription start?',
     options: ['Enter date in format dd-mm-yyyy'],
     validation: new RegExp(
       /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/,
